7a6b7c2e9f72285d99fbfc6957f8761cdf870139
Moasfar Javed <88271241+Moasfar-Javed@users.noreply.github.com>
2024-06-13 13:25:12 +0500

    water off logic moved to server from hardware

--- src/utility/websocket_utility.mjs
+++ src/utility/websocket_utility.mjs
@@ -8,26 +8,14 @@ const initializeWebSocketServer = (wsPort) => {
 
   wss.on("connection", (ws, req) => {
     console.log("WebSocket connected");
-    // ws.on("message", (message) => {
-    //   const data = JSON.parse(message);
-    //   console.log(data.arduino_id);
-    //  , "water_on": false
-    // });
+
     ws.on("message", async (message) => {
       try {
         const data = JSON.parse(message);
         console.log(data);
-        // if (!clients[data.arduino_id]) {
+
         clients[data.arduino_id] = ws;
         console.log(`Client with arduino_id ${data.arduino_id} connected.`);
-        // }
-
-        if (data.hasOwnProperty("water_on") && data.water_on === false) {
-          await IrrigationService.toggleWaterOff(data.arduino_id);
-          console.log(
-            `Client with arduino_id ${data.arduino_id} has water_on set to off.`
-          );
-        }
       } catch (error) {
         console.log("Error parsing message:", error);
       }
@@ -39,9 +27,14 @@ const initializeWebSocketServer = (wsPort) => {
 
 const sendMessageToClient = (arduino_id, payload) => {
   const client = clients[arduino_id];
-  //console.log(client.readyState);
   if (client && client.readyState === 1) {
     client.send(JSON.stringify({ payload }));
+    setTimeout(async () => {
+      await IrrigationService.toggleWaterOff(arduino_id);
+      console.log(
+        `Client with arduino_id ${arduino_id} has water_on set to off.`
+      );
+    }, payload.duration * 60000);
     return true;
   } else {
     return false;
